a2454b33e31f9b39fc9918eb5af51475bacd6bbe
MD MUBARAK HOSSAIN <hossainmubarak477@gmail.com>
2024-02-25 14:30:10 +0600

    added document

--- src/documentation/home/Home.js
+++ src/documentation/home/Home.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import Button from '../../utilities/button/Button';
-import Loading from '../../utilities/Spiner/Loading';
+import Features from './Features';
+import Offere from './Offere';
 const Home = () => {
   return (
-    <div className="hero min-h-screen bg-base-100">
+    <div className="min-h-screen bg-base-100">
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">Hello there</h1>
           <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-          <Loading />
-          <Button />
         </div>
+      </div>
+      <div>
+        <Features />
+      </div>
+      <div>
+        <Offere />
       </div>
     </div>
 

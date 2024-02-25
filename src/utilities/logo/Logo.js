b92e4a411bcaf1cb59ee54c42ef3c2f2c1eb5d57
MD MUBARAK HOSSAIN <hossainmubarak477@gmail.com>
2024-02-25 11:21:07 +0600

    added logo upadet

--- src/utilities/logo/Logo.js
+++ src/utilities/logo/Logo.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const Logo = () => <Link to='/' className="text-purple-600 text-xl uppercase h-20"><span>phd papers</span></Link>
+const Logo = () => <Link to='/' className="text-purple-600 lg:text-3xl uppercase h-16 font-bold"><span>phd papers</span></Link>
 export default Logo;

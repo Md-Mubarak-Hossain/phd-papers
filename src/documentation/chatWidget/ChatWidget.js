20a186dfb96889e401ce03077be20386a963a08c
MD MUBARAK HOSSAIN <hossainmubarak477@gmail.com>
2024-02-25 13:04:44 +0600

    added document

--- src/documentation/chatWidget/ChatWidget.js
+++ src/documentation/chatWidget/ChatWidget.js
@@ -6,9 +6,11 @@ const ChatWidget = () => {
             {/* ......................
             Open the modal using document.getElementById('ID').showModal() method 
             ...........................*/}
-            <button className="btn btn-primary shadow-lg p-2 w-24" onClick={() => document.getElementById('my_modal_1').showModal()}>Chat Us!</button>
-            <dialog id="my_modal_1" className="modal">
-                <div className="modal-box bg-slate-50 text-black">
+            <button className="btn btn-primary shadow-lg p-2 w-28" onClick={() => document.getElementById('my_modal_1').showModal()}>Live Chat Us!</button>
+            <dialog id="my_modal_1" className="modal h-full w-96 bg-white text-black border-1 border-cyan-100 ms-auto">
+                
+                <div className="modal-box shadow-none h-full">
+                <div className='h-5 bg-primary w-full'>&nbsp;</div>
                     {/*................
                      Chat section start
                      ..................*/}
@@ -16,15 +18,15 @@ const ChatWidget = () => {
                     {/*.................
                      Chat section end 
                      ...................*/}
-                    <div className="modal-action">
+                </div>
+                    <div className="modal-action ms-auto mt-auto">
                         <form method="dialog">
                             {/*......................
                              if there is a button in form, it will close the modal 
                              .........................*/}
-                            <button className="btn">Exit</button>
+                            <button className="btn btn-sm btn-primary">Close chats</button>
                         </form>
                     </div>
-                </div>
             </dialog>
         </>
     );

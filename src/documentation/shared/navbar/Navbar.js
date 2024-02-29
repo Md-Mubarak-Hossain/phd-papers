71841a64e5161ce45f200ad4c8dbbcc3ec883ce1
MD MUBARAK HOSSAIN <hossainmubarak477@gmail.com>
2024-03-01 02:43:29 +0600

    added some page

--- src/documentation/shared/navbar/Navbar.js
+++ src/documentation/shared/navbar/Navbar.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ExternalLink } from 'react-external-link';
-import resultImg from '../../../components/images/allResultLogo.png'
+import resultImg from '../../../components/images/allResultLogo.png';
+import electionNID from '../../../components/images/nid.png';
+
 const Navbar = () => {
   return (
-    <ul className="menu w-40  lg:fixed">
+    <ul className="menu w-40  lg:fixed text-primary">
       <li>
         <Link to='/'>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
           Home
         </Link >
       </li>
-      <li className='text-primary'>
-
+      <li>
         <ExternalLink
           href="http://www.educationboardresults.gov.bd/">
           <img
@@ -23,10 +24,13 @@ const Navbar = () => {
 
       </li>
       <li>
-        <Link to='/'>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
-          Item 3
-        </Link >
+        <ExternalLink
+          href="https://www.nidw.gov.bd/">
+          <img
+            className='h-7'
+            src={electionNID} alt="NID of BD"/>
+          NID of Bangladesh
+        </ExternalLink>
       </li>
     </ul>
   );
